2554fe4a6ac441e14d20f575fd1ff0a91c09ab9c
monkeygate <monkeygate@hotmail.com>
2016-03-29 10:25:06 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,8 @@
-file { '/etc/skel/.bashrc':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    source  => 'puppet:///modules/skeleton/.bashrc',
-  }
+class skeleton {
+    file { '/etc/skel/.bashrc':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/skeleton/.bashrc',
+      }
+}
