043590a8f1cdb0b4e657484f0dd9490d42a83c81
monkeygate <monkeygate@hotmail.com>
2016-03-29 13:41:12 -0700

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,24 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure  => directory,
+  }
+    
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    # subscribe => File['/etc/nginx/nginx.conf'],
+  }
+}
