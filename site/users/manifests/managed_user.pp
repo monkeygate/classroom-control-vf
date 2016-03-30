a1daea18674d7d8d2f84d33c5d5da8ea08c22e5b
monkeygate <monkeygate@hotmail.com>
2016-03-30 13:39:46 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -2,4 +2,10 @@ define users::managed_user (
     $user = $title,
     $homedir     = '/var/log/httpd',
     $sshdir     = '/var/ssh',
- )
+ ){
+  user { "${title}":
+    ensure  => user,
+    homedir   => $homedir,
+    sshdir   => $sshdir,
+  }
+}
