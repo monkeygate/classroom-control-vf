62b8446a6fb640930ba3e01dc1870745d95d8c99
monkeygate <monkeygate@hotmail.com>
2016-03-30 15:04:35 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -13,24 +13,24 @@ class nginx {
     }
   }
 
-  $www_path = '/var/www'
-  $etc_nginx_path = '/etc/nginx'
+  # $www_path = '/var/www'
+  # $etc_nginx_path = '/etc/nginx'
   $modules_nginx_path = '/modules/nginx'
   
   package { 'nginx':
     ensure => installed,
   }
   
-  file { "${www_path}":
+  file { "${document_root}":
     ensure  => directory,
   }
   
-  file { "${www_path}/index.html":
+  file { "${document_root}/index.html":
     ensure  => file,
     source => "puppet://${modules_nginx_path}/index.html",
   }
     
-  file { "${etc_nginx_path}/nginx.conf":
+  file { "${config_directory}/nginx.conf":
     ensure  => file,
     source  => "puppet://${modules_nginx_path}/nginx.conf",
     require => Package['nginx'],
@@ -40,6 +40,6 @@ class nginx {
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    # subscribe => File["${etc_nginx_path}/nginx.conf"],
+    # subscribe => File["${config_directory}/nginx.conf"],
   }
 }
