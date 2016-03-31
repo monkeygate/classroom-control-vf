531d2c71e65318f313e9b5cd877a15dca0a5b63d
monkeygate <monkeygate@hotmail.com>
2016-03-31 10:36:33 -0700

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,25 +1,28 @@
 class nginx::params {
   case $::osfamily {
-    'RedHat': {
-       $httpd_user    = 'apache'
-       $httpd_group   = 'apache'
-       $httpd_pkg     = 'httpd'
-       $httpd_svc     = 'httpd'
-       $httpd_conf    = 'httpd.conf'
-       $httpd_confdir = '/etc/httpd/conf'
-       $httpd_docroot = '/var/www/html'
+    'redhat', 'debian': {
+      $package = 'nginx'
+      $owner   = 'root'
+      $group   = 'root'
+      $confdir = '/etc/nginx'
+      $logdir  = '/var/log/nginx'
+      $docroot = '/var/www'
     }
-    'Debian': {
-       $httpd_user    = 'www-data'
-       $httpd_group   = 'www-data'
-       $httpd_pkg     = 'apache2'
-       $httpd_svc     = 'apache2'
-       $httpd_conf    = 'apache2.conf'
-       $httpd_confdir = '/etc/apache2'
-       $httpd_docroot = '/var/www'
+    'windows': {
+      $package = 'nginx-service'
+      $owner   = 'Administrator'
+      $group   = 'Administrators'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir  = 'C:/ProgramData/nginx/logs'
+      $docroot = 'C:/ProgramData/nginx/html'
     }
     default: {
       fail("Module ${module_name} is not supported on ${::osfamily}")
     }
+    $user = $::osfamily ? {
+      'redhat'  => 'nginx',
+      'debian'  => 'www-data',
+      'windows' => 'nobody',
+    }    
   }
 }
